ddb0320e883e685222c075eca7a887a59b36d77b
dannylmathews <dannyl.mathews@gmail.com>
2014-09-28 09:18:37 -0700

    started project init

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C-Starter-Project
+//
+//  Created by Daniel Mathews on 2014-09-28.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    printf("I'm starting the iOS prep course.\n");
+    return 0;
+}
